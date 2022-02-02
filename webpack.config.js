d77ea533755a0054500c5cb1a3744f09c2862e5b
Will Linux <druids50@gmail.com>
2022-02-02 12:08:17 -0600

    webpack and various stuff added

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Todo List',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
