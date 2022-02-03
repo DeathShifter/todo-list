6b27638d817df0a0b3d935ff6df988a5f5cde246
Will Linux <druids50@gmail.com>
2022-02-03 12:19:25 -0600

    im done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
-import _ from 'lodash';
+import { format, compareAsc, isToday } from "date-fns";
+import datepicker from 'js-datepicker';
 
- function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+const todo = (title, dueDate) => {
+	const say = () => console.log(`${title}, ${dueDate}`);
+	return { title, dueDate, say };
+};
 
-    btn.innerHTML = 'Click me and check the console!';
+const walkDogs = todo("walkDogs", isToday(isToday));
 
-    element.appendChild(btn);
+walkDogs.say();
 
-    return element;
- }
+const todoList = [];
 
- document.body.appendChild(component());
+const selectDiv = document.getElementById('displaySection');
+const input = document.createElement('input');
+const btnAdd = document.createElement('button');
+const btnCancel = document.createElement('button');
+const div = document.createElement('div');
+
+btnAdd.innerHTML = "Add";
+btnCancel.innerHTML = "Cancel";
+
+selectDiv.appendChild(input);
+selectDiv.appendChild(btnAdd);
+selectDiv.appendChild(btnCancel);
+selectDiv.appendChild(div)
+
+btnAdd.addEventListener('click', function() {
+	if (input.value != '') {
+		todoList.push(input.value);
+		input.value = "";
+		div.innerHTML += `<div> ${todoList.at(-1)} </div>`;
+		console.log(todoList);
+
+	}
+});
+
+btnCancel.addEventListener('click', function() {
+	input.value = "";
+});
+
+for (let i = 0; i < todoList.length; i++) {
+	selectDiv.appendChild(div);
+};
+
+// const addItem = () => {
+// 	const btn = document.createElement('button');
+// 	btn.innerHTML = "Add item";
+// 	document.body.appendChild(btn);
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+    },
     mode: 'development',
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Todo List',
-        }),
-    ],
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+    }),
+],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };
